78baa4f11b06b72b26d485b322e05387989a3e0f
userscr <sebastiancano21@hotmail.es>
2023-08-03 22:34:35 -0500

    about y color

--- movie/views.py
+++ movie/views.py
@@ -7,3 +7,6 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name': 'Sebastian Cano Rincon'})
+
+def about(request):
+    return render(request, 'about.html')
